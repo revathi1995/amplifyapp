c85240b9a786d05417aa90d8d158c96922f9f1ee
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-10-28 18:23:33 +0530

    Update face.js

--- src/api/face.js
+++ src/api/face.js
@@ -1,30 +1,23 @@
-import * as faceapi from 'face-api.js';
-
+import * as faceapi from 'face-api.js'
 
 export async function loadModels() {
-  const MODEL_URL = process.env.PUBLIC_URL + 'public/models';
-  await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
-  await faceapi.loadFaceLandmarkTinyModel(MODEL_URL);
-  await faceapi.loadFaceRecognitionModel(MODEL_URL);
+    const MODEL_URL = process.env.PUBLIC_URL + '/models'
+    await faceapi.loadTinyFaceDetectorModel(MODEL_URL)
+    await faceapi.loadFaceLandmarkTinyModel(MODEL_URL)
+    await faceapi.loadFaceRecognitionModel(MODEL_URL)
 }
 
 export async function getFullFaceDescription(blob, inputSize = 512) {
-
-  let scoreThreshold = 0.5;
-  const OPTION = new faceapi.TinyFaceDetectorOptions({
-    inputSize,
-    scoreThreshold
-  });
-  const useTinyModel = true;
-
-
-  let img = await faceapi.fetchImage(blob);
-
-  let fullDesc = await faceapi
-    .detectAllFaces(img, OPTION)
-    .withFaceLandmarks(useTinyModel)
-    .withFaceDescriptors();
-  return fullDesc;
+    const scoreThreshold = 0.5
+    const OPTION = new faceapi.TinyFaceDetectorOptions({
+        inputSize,
+        scoreThreshold
+    })
+    const useTinyModel = true
+    const img = await faceapi.fetchImage(blob)
+    const fullDesc = await faceapi
+        .detectAllFaces(img, OPTION)
+        .withFaceLandmarks(useTinyModel)
+        .withFaceDescriptors()
 }
-
 
