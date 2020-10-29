e4059f3eae831f1cc07cf09ffcd011f5b296e647
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-10-29 11:28:17 +0530

    Update face.js

--- src/api/face.js
+++ src/api/face.js
@@ -1,22 +1,17 @@
 import * as faceapi from 'face-api.js'
 
 export async function loadModels() {
-  try {
-          await faceapi.nets.TinyFaceDetector.loadFromUri('public/models');
+  
           await faceapi.nets.ssdMobilenetv1.loadFromUri('public/models');
           await faceapi.nets.faceLandmark68Net.loadFromUri('public/models');
           await faceapi.nets.faceRecognitionNet.loadFromUri('public/models');
 
-
-        } catch (error) {
-          console.log(error);
-        }
 }
 
 export async function getFullFaceDescription(blob, inputSize = 512) {
 
   let scoreThreshold = 0.5;
-  const OPTION = new faceapi.ssdMobilenetv1Options({
+  const OPTION = new faceapi.TinyFaceDetectorOptions({
     inputSize,
     scoreThreshold
   });
