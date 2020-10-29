e5bba659e401b06c92bea1e79792712948e717cc
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-10-29 09:36:13 +0530

    Update face.js

--- src/api/face.js
+++ src/api/face.js
@@ -1,23 +1,31 @@
 import * as faceapi from 'face-api.js'
 
-export async function loadModels() {
-    const MODEL_URL = process.env.PUBLIC_URL + '/public/models'
-    await faceapi.loadTinyFaceDetectorModel(MODEL_URL)
-    await faceapi.loadFaceLandmarkTinyModel(MODEL_URL)
-    await faceapi.loadFaceRecognitionModel(MODEL_URL)
-}
+async function updateResults() {
+      if (!isFaceDetectionModelLoaded()) {
+        return
+      }
 
-export async function getFullFaceDescription(blob, inputSize = 512) {
-    const scoreThreshold = 0.5
-    const OPTION = new faceapi.TinyFaceDetectorOptions({
-        inputSize,
-        scoreThreshold
+      const inputImgEl = $('#inputImg').get(0)
+      const options = getFaceDetectorOptions()
+
+      const results = await faceapi.detectAllFaces(inputImgEl, options)
+
+      const canvas = $('#overlay').get(0)
+      faceapi.matchDimensions(canvas, inputImgEl)
+      faceapi.draw.drawDetections(canvas, faceapi.resizeResults(results, inputImgEl))
+    }
+
+    async function run() {
+      // load face detection
+      await changeFaceDetector(SSD_MOBILENETV1)
+
+      // start processing image
+      updateResults()
+    }
+
+    $(document).ready(function() {
+      renderNavBar('#navbar', 'face_detection')
+      initImageSelectionControls()
+      initFaceDetectionControls()
+      run()
     })
-    const useTinyModel = true
-    const img = await faceapi.fetchImage(blob)
-    const fullDesc = await faceapi
-        .detectAllFaces(img, OPTION)
-        .withFaceLandmarks(useTinyModel)
-        .withFaceDescriptors()
-}
-
