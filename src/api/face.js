bc51730eed63f06391016b9737e3110c39edf423
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-10-29 09:41:54 +0530

    Update face.js

--- src/api/face.js
+++ src/api/face.js
@@ -1,31 +1,54 @@
 import * as faceapi from 'face-api.js'
 
-async function updateResults() {
-      if (!isFaceDetectionModelLoaded()) {
-        return
-      }
+let forwardTimes = []
 
-      const inputImgEl = $('#inputImg').get(0)
+    function updateTimeStats(timeInMs) {
+      forwardTimes = [timeInMs].concat(forwardTimes).slice(0, 30)
+      const avgTimeInMs = forwardTimes.reduce((total, t) => total + t) / forwardTimes.length
+      $('#time').val(`${Math.round(avgTimeInMs)} ms`)
+      $('#fps').val(`${faceapi.utils.round(1000 / avgTimeInMs)}`)
+    }
+
+    async function onPlay() {
+      const videoEl = $('#inputVideo').get(0)
+
+      if(videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded())
+        return setTimeout(() => onPlay())
+
+
       const options = getFaceDetectorOptions()
 
-      const results = await faceapi.detectAllFaces(inputImgEl, options)
+      const ts = Date.now()
 
-      const canvas = $('#overlay').get(0)
-      faceapi.matchDimensions(canvas, inputImgEl)
-      faceapi.draw.drawDetections(canvas, faceapi.resizeResults(results, inputImgEl))
+      const result = await faceapi.detectSingleFace(videoEl, options)
+
+      updateTimeStats(Date.now() - ts)
+
+      if (result) {
+        const canvas = $('#overlay').get(0)
+        const dims = faceapi.matchDimensions(canvas, videoEl, true)
+        faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims))
+      }
+
+      setTimeout(() => onPlay())
     }
 
     async function run() {
-      // load face detection
-      await changeFaceDetector(SSD_MOBILENETV1)
+      // load face detection model
+      await changeFaceDetector(TINY_FACE_DETECTOR)
+      changeInputSize(128)
 
-      // start processing image
-      updateResults()
+      // try to access users webcam and stream the images
+      // to the video element
+      const stream = await navigator.mediaDevices.getUserMedia({ video: {} })
+      const videoEl = $('#inputVideo').get(0)
+      videoEl.srcObject = stream
     }
 
+    function updateResults() {}
+
     $(document).ready(function() {
-      renderNavBar('#navbar', 'face_detection')
-      initImageSelectionControls()
+      renderNavBar('#navbar', 'webcam_face_detection')
       initFaceDetectionControls()
       run()
     })
